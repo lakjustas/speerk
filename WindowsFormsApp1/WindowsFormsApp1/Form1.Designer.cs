f7a91dd7a60245683b42520e95b096d7d74d30e0
LakyJL <justas0333@gmail.com>
2017-09-20 20:13:33 +0300

    main project files

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibProcessed = new Emgu.CV.UI.ImageBox();
+            this.btnPauseOrResume = new System.Windows.Forms.Button();
+            this.txtXYRadius = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.Location = new System.Drawing.Point(3, 2);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(640, 480);
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibProcessed
+            // 
+            this.ibProcessed.Location = new System.Drawing.Point(649, 2);
+            this.ibProcessed.Name = "ibProcessed";
+            this.ibProcessed.Size = new System.Drawing.Size(640, 480);
+            this.ibProcessed.TabIndex = 2;
+            this.ibProcessed.TabStop = false;
+            // 
+            // btnPauseOrResume
+            // 
+            this.btnPauseOrResume.Location = new System.Drawing.Point(89, 530);
+            this.btnPauseOrResume.Name = "btnPauseOrResume";
+            this.btnPauseOrResume.Size = new System.Drawing.Size(123, 56);
+            this.btnPauseOrResume.TabIndex = 3;
+            this.btnPauseOrResume.Text = "pause";
+            this.btnPauseOrResume.UseVisualStyleBackColor = true;
+            this.btnPauseOrResume.Click += new System.EventHandler(this.btnPauseOrResume_Click);
+            // 
+            // txtXYRadius
+            // 
+            this.txtXYRadius.Location = new System.Drawing.Point(357, 506);
+            this.txtXYRadius.Multiline = true;
+            this.txtXYRadius.Name = "txtXYRadius";
+            this.txtXYRadius.ReadOnly = true;
+            this.txtXYRadius.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtXYRadius.Size = new System.Drawing.Size(932, 127);
+            this.txtXYRadius.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 636);
+            this.Controls.Add(this.txtXYRadius);
+            this.Controls.Add(this.btnPauseOrResume);
+            this.Controls.Add(this.ibProcessed);
+            this.Controls.Add(this.ibOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibProcessed;
+        private System.Windows.Forms.Button btnPauseOrResume;
+        private System.Windows.Forms.TextBox txtXYRadius;
+    }
+}
+
