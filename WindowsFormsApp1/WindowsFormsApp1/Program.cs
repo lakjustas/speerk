4ac338ddc2b6e19dbfb4248c7d60a543e35eb43a
lakjustas <lakjustas@gmail.com>
2017-09-23 13:28:17 +0300

    comm delete

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -11,7 +11,6 @@ namespace WindowsFormsApp1
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// Comments
         [STAThread]
         static void Main()
         {
