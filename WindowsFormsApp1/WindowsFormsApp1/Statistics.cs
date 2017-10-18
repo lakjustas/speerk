32deb49988af23396d6e8a1e3e777239eb13e046
Šarūnas <sarunas.bagdonavicius@gmail.com>
2017-10-18 16:16:13 +0300

    New class Statistics

---
+++ WindowsFormsApp1/WindowsFormsApp1/Statistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Statistics
+    {
+        String name1;
+        String name2;
+        int Score1;
+        int Score2;
+
+    }
+}
