4304a333951d8e98fe2c1583f22ddce35e849a4f
Šarūnas <sarunas.bagdonavicius@gmail.com>
2017-10-18 23:30:49 +0300

    Saving match statistics to file

--- WindowsFormsApp1/WindowsFormsApp1/Statistics.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Statistics.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
-    class Statistics
+    public class Statistics
     {
-        private String name1;
-        private String name2;
-        private int Score1;
-        private int Score2;
-        private DateTime date;
+        public String name1;
+        public String name2;
+        public int score1;
+        public int score2;
+        public DateTime date;
 
         public Statistics()
         {
@@ -21,8 +23,8 @@ namespace WindowsFormsApp1
         
         public void SetScores(int score1, int score2)
         {
-            this.Score1 = score1;
-            this.Score2 = score2;
+            this.score1 = score1;
+            this.score2 = score2;
         }
 
         public void SetNames(String name1, String name2)
@@ -32,5 +34,13 @@ namespace WindowsFormsApp1
         }
 
         public void WriteToFile()
+        {
+            FileStream fileStream = new FileStream("Statistics.xml", FileMode.Append);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Statistics));
+            xmlSerializer.Serialize(streamWriter, this);
+            streamWriter.Close();
+            fileStream.Close();
+        }
     }
 }
