e9addd1fdf15486d9f8063f122b91c52468f1fb5
kaspgreta <greta.kasparaityte@gmail.com>
2017-10-19 10:58:07 +0300

    UI tobulinimas (#13)
    * README failo užpildymas
    
    * Sumažinau eilučių skaičių. Labai sunku skaityt kodą, kai daug enter'ių. :(
    
    * UI'o pradžia
    
    * UI
    
    * Form1 keitimas
    
    * Didesnis FORM1 langas

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -24,8 +24,8 @@ namespace WindowsFormsApp1
         BallTracker ballTracker = new BallTracker();
         GateTracker gateTracker = new GateTracker();
         bool blnCapturingInProcess = false;
-        Team teamLeft = new Team("Kairioji komanda", 0);
-        Team teamRight = new Team("Dešinioji komanda", 0);
+        Team teamLeft = new Team("", 0);
+        Team teamRight = new Team("", 0);
         bool goal = false;
         String videoFileDir;
 
@@ -47,8 +47,8 @@ namespace WindowsFormsApp1
                 return;
             }*/
 
-            teamLeftBox.AppendText(teamLeft.GetName() + ": " + teamLeft.Score.ToString());
-            teamRightBox.AppendText(teamRight.GetName() + ": " + teamRight.Score.ToString());
+            teamLeftBox.AppendText(teamLeft.GetName() + "" + teamLeft.Score.ToString());
+            teamRightBox.AppendText(teamRight.GetName() + "" + teamRight.Score.ToString());
 
             /*Application.Idle += ProcessFrameAndUpdateGUI;
             blnCapturingInProcess = true;*/
@@ -156,13 +156,13 @@ namespace WindowsFormsApp1
             {
                 Application.Idle -= ProcessFrameAndUpdateGUI;
                 blnCapturingInProcess = false;
-                btnPauseOrResume.Text = "Resume";
+                btnPauseOrResume.Text = "Tęsti";
             }
             else
             {
                 Application.Idle += ProcessFrameAndUpdateGUI;
                 blnCapturingInProcess = true;
-                btnPauseOrResume.Text = "Pause";
+                btnPauseOrResume.Text = "Pauzė";
             }
         }
 
@@ -192,6 +192,11 @@ namespace WindowsFormsApp1
                 txtXYRadius.Text = except.Message;
                 return;
             }
+        }
+
+        private void teamLeftBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
