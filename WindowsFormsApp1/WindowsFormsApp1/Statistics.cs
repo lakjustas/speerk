7d474c36af186309edc7798e289d6d473498802b
lakjustas <31969313+lakjustas@users.noreply.github.com>
2017-10-19 15:39:18 +0300

    SetScoresTests (#15)
    * SetScoresTests
    
    * SetName tests

--- WindowsFormsApp1/WindowsFormsApp1/Statistics.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Statistics.cs
@@ -26,11 +26,34 @@ namespace WindowsFormsApp1
             this.score1 = score1;
             this.score2 = score2;
         }
+        
+        public void SetScores() {} // For tests, if values ar not given
+
+        public int getScore1()
+        {
+            return score1;
+        }
+
+        public int getScore2()
+        {
+            return score2;
+        }
+
 
         public void SetNames(String name1, String name2)
         {
             this.name1 = name1;
             this.name2 = name2;
+        }
+
+        public string getName1()
+        {
+            return name1;
+        }
+
+        public string getName2()
+        {
+            return name2;
         }
 
         public void WriteToFile(List<Statistics> stats)
