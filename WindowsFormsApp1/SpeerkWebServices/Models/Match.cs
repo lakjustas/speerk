217eb5e8eecdb0cfc520cb43ce3f7fb0e29a3e7a
lakjustas <lakjustas@gmail.com>
2017-11-08 19:54:24 +0200

    added match props

---
+++ WindowsFormsApp1/SpeerkWebServices/Models/Match.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpeerkWebServices.Models
+{
+    public class Match
+    {
+        public int ID { get; set; }
+
+        public string name1 { get; set; }
+
+        public string name2 { get; set; }
+
+        public int score1 { get; set; }
+
+        public int score2 { get; set; }
+
+        public DateTime date { get; set; }
+
+    }
+}
