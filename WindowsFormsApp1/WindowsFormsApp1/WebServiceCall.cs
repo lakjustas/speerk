3693cbb9ff125289d1a355069ef0b1e17b64b880
Šarūnas <sarunas.bagdonavicius@gmail.com>
2017-11-23 11:49:59 +0200

    Get by id method

--- WindowsFormsApp1/WindowsFormsApp1/WebServiceCall.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WebServiceCall.cs
@@ -67,6 +67,23 @@ namespace WindowsFormsApp1
 
 
         }
+
+        public Matches GET(int id)
+        {
+            Matches match;
+            string url = "http://localhost:56233/api/Match/" + id.ToString();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                string json = reader.ReadToEnd();
+
+                match = JsonConvert.DeserializeObject<Matches>(json);
+                return match;
+
+            }
+        }
     }
 
 
