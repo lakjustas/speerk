e6b97846460f4c6770ab091f6db71dfda52cef54
greta.kasparaityte <greta.kasparaityte@mif.stud.vu.lt>
2017-12-14 20:19:27 +0200

    Dizainas

--- WindowsFormsApp1/SpeerkMobileApp/Resources/Resource.Designer.cs
+++ WindowsFormsApp1/SpeerkMobileApp/Resources/Resource.Designer.cs
@@ -1573,10 +1573,16 @@ namespace SpeerkMobileApp
 			public const int background = 2130837587;
 			
 			// aapt resource value: 0x7f020054
-			public const int Kamuolys = 2130837588;
+			public const int Fiksavimas = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int notification_template_icon_bg = 2130837589;
+			public const int Fiksavimas2 = 2130837589;
+			
+			// aapt resource value: 0x7f020056
+			public const int Kamuolys = 2130837590;
+			
+			// aapt resource value: 0x7f020057
+			public const int notification_template_icon_bg = 2130837591;
 			
 			static Drawable()
 			{
